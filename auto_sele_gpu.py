a58af58477468ebaf556f950f6405510846be612
ykq <ykq@users.noreply.github.com>
2020-10-07 21:38:58 +0800

    添加注释

--- auto_sele_gpu.py
+++ auto_sele_gpu.py
@@ -4,25 +4,25 @@ import time
  
 cmd = 'python ~/mytrain.py'  # 运行代码命令
  
- 
+# 获取GPU状态信息
 def gpu_info():
-    gpu_status = os.popen('nvidia-smi | grep %').read().split('|')
+    gpu_status = os.popen('nvidia-smi -i 3 | grep %').read().split('|')  # 指定gpu
     gpu_memory = int(gpu_status[2].split('/')[0].split('M')[0].strip())
     gpu_power = int(gpu_status[1].split('   ')[-1].split('/')[0].split('W')[0].strip())
     return gpu_power, gpu_memory
  
- 
-def narrow_setup(interval=2):
+
+def auto_train(interval=2):
     gpu_power, gpu_memory = gpu_info()
     i = 0
-    while gpu_memory > 1000 or gpu_power > 20:  # set waiting condition
+    while gpu_memory > 1000 or gpu_power > 20:  # 根据GPU的显存和功率设置等待条件
         gpu_power, gpu_memory = gpu_info()
         i = i % 5
         symbol = 'monitoring: ' + '>' * i + ' ' * (10 - i - 1) + '|'
         gpu_power_str = 'gpu power:%d W |' % gpu_power
         gpu_memory_str = 'gpu memory:%d MiB |' % gpu_memory
         sys.stdout.write('\r' + gpu_memory_str + ' ' + gpu_power_str + ' ' + symbol)
-        sys.stdout.flush()
+        sys.stdout.flush()  # 打印当前gpu状态
         time.sleep(interval)
         i += 1
     print('\n' + cmd)
@@ -30,4 +30,4 @@ def narrow_setup(interval=2):
  
  
 if __name__ == '__main__':
-    narrow_setup()
+    auto_train()
